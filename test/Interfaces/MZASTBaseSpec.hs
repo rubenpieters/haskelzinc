35bee3e70a4822d04403cb14d2810eef752aa1c9
ruben <rubnepieters@gmail.com>
2017-02-15 22:05:29 +0100

    initial try quickcheck

--- test/Interfaces/MZASTBaseSpec.hs
+++ test/Interfaces/MZASTBaseSpec.hs
@@ -1,12 +1,19 @@
 module Interfaces.MZASTBaseSpec (main, spec) where
 
+import Interfaces.MZASTBase
+
 import Test.Hspec
+import Test.QuickCheck
+
+import Data.DeriveTH
+
+derive makeArbitrary ''Expr
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "temporary sanity check test" $ do
-    it "should pass" $ do
-      "a" `shouldBe` "a"
+  describe "stripExprOff" $ do
+    it "should strip the annotations from an expression" $ property $
+      \e ans -> stripExprOff (AnnExpr e ans) `shouldBe` e
